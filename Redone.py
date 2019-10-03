42e4c2fa664b7af4480ac5f7774aa0b8547eee95
At1shaySharma <53185104+At1shaySharma@users.noreply.github.com>
2019-10-03 19:13:42 +0530

    Add files via upload

---
+++ Redone.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    N = int(input())
+    L = [1]
+    if N == 1:
+        print(1)
+    else:
+        for i in range(1, N):
+            L.append((L[-1]*(i+2)) + (i+1))
+        print(L[-1]%1000000007)
